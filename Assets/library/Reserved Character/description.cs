b06c64d4da4492847e57fb08ea346a26099fbef9
yungcheukhin <yungcheukhin@gmail.com>
2016-10-26 17:05:58 +0800

    All asset in organized grouping

---
+++ Assets/library/Reserved Character/description.cs
@@ -0,0 +1,8 @@
+﻿/*
+ *  Character models in this folder are unused, but may be useful in the future 
+ *  Some character have defined motion pack
+ *  While some of them do not
+ *  All divided into 2 folders
+ *  raw character
+ *  defined character
+ */
