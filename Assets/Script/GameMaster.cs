1338bb42c9796078af62d3ca47f3f8be31301029
yungcheukhin <yungcheukhin@gmail.com>
2016-11-25 16:26:26 +0800

    creating Maze

--- Assets/Script/GameMaster.cs
+++ Assets/Script/GameMaster.cs
@@ -7,11 +7,11 @@ public class GameMaster : MonoBehaviour {
     public float offsetY = 40;
     public float sizeX  = 100;
     public float sizeY = 40;
-
     public Transform musicPrefab;
-    
+    bool RestartFlag = false;   //true if the game 
+
 	// Update is called once per frame
-	void Start ()
+	private void Start ()
     {
        if(!GameObject.FindGameObjectWithTag("MM"))
         {
@@ -19,11 +19,22 @@ public class GameMaster : MonoBehaviour {
             mManger.name = musicPrefab.name;
             DontDestroyOnLoad(mManger);
         }
+        CreateMaze();
 	}
+    
+    private void Update()
+    {
+        
+    } 
 
     void OnGUI()
     {
         int currentHealth = GameObject.Find("MAX").GetComponent<CharacterHealth>().current_health;
         GUI.Box(new Rect(Screen.width / 2 - sizeX / 2, offsetY, sizeX, sizeY), "Health: " + currentHealth);
     }
+
+    private void CreateMaze() { }   //generate Maze
+    private void ResetMaze() { }    //destory maze if it needa restart game
+    public void setRestart() { }    //
+
 }
