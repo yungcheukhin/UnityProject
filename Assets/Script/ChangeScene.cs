34796ac661476f9c772faa9f275f38045e51b567
Ng Eris <fly3eris@hotmail.com>
2016-11-25 18:18:14 +0800

    Revert "save"
    This reverts commit 80de5d42549a2dc8ad0671c850739540a986c75c.

--- Assets/Script/ChangeScene.cs
+++ Assets/Script/ChangeScene.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
-using UnityEditor.SceneManagement;
 using System.Collections;
 
 public class ChangeScene : MonoBehaviour {
 
 public void ChangeToScene(int sceneNumber)
     {
-        string myName = GameObject.FindWithTag("Name").GetComponent<UnityEngine.UI.Text>().text;
-        PlayerPrefs.SetString("player_name", myName);
-        EditorSceneManager.LoadScene(sceneNumber);
+        Application.LoadLevel(sceneNumber);
     }
 }
