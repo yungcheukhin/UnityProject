2da11d7ab1ef3f8bc10d19892b6f429f6a4db499
Ng Eris <fly3eris@hotmail.com>
2016-11-28 16:47:45 +0800

    9mit

--- Assets/testControl.cs
+++ Assets/testControl.cs
@@ -11,43 +11,4 @@ public class testControl : MonoBehaviour
         run_animation = "Run",
         walk_animation = "Walk";
 
-    Vector3 Stand = new Vector3(0, 0, 0);
-
-    float stopTime;
-    float moveTime;
-    int curr_frame;
-    int total_frame;
-    float timeCounter1;
-    float timeCounter2;
-    float vel_x, vel_y, vel_z;
-
-    void Update()
-    {
-        timeCounter1 += Time.deltaTime;
-        if (timeCounter1 < moveTime)
-        {
-            transform.Translate(vel_x, vel_y, 0, Space.Self);
-            anim.CrossFade(walk_animation);
-        }
-        else
-        {
-            timeCounter2 += Time.deltaTime;
-            if (timeCounter2 > stopTime)
-            {
-                Change();
-                timeCounter1 = 0;
-                timeCounter2 = 0;
-            }
-        }
-        //Check();
-    }
-    void Change()
-    {
-        stopTime = Random.Range(1, 5);
-        moveTime = Random.Range(1, 20);
-        vel_x = Random.Range(1, 10);
-        vel_y = Random.Range(1, 10);
-    }
-
-
 }
