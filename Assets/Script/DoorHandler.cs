82a6c892b26331299c495248a6ebdfa89fecc410
wykitty <waiyikitty@gmail.com>
2016-11-28 20:36:04 +0800

    added doorhandles script

---
+++ Assets/Script/DoorHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorHandler : MonoBehaviour {
+
+	private Animator _animator = null;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter(Collider collider){
+		_animator.SetBool ("isopen", true);
+	}
+
+	void OnTriggerExit(Collider collider){
+		_animator.SetBool ("isopen", false);
+	}
+
+}
