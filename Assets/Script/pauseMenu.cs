d3f15167bf6d8e376f893d791c785bcb63982a55
zoefishxd <xd.zoe.307@gmail.com>
2017-03-05 18:28:34 +0800

    Pause Manu

---
+++ Assets/Script/pauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour {
+
+    public GameObject menu;
+    public bool isShowing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isShowing == false)
+        {
+            menu.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isShowing == false)
+            {
+                Time.timeScale = 0.0f;
+                menu.SetActive(true);
+                isShowing = true;
+            }
+            else
+            {
+                Time.timeScale = 1.0f;
+                menu.SetActive(false);
+                isShowing = false;
+            }
+        }
+	}
+
+    public void resume()
+    {
+        Time.timeScale = 1.0f;
+        menu.SetActive(false);
+        isShowing = false;
+    }
+}
