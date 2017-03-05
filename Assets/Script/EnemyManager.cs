869065212e5221d7ea0ec2a5fe0793b0d7295bce
Ng Eris <fly3eris@hotmail.com>
2017-03-05 18:36:56 +0800

    輕輕一推

--- Assets/Script/EnemyManager.cs
+++ Assets/Script/EnemyManager.cs
@@ -7,8 +7,14 @@ public class EnemyManager : MonoBehaviour {
     private CharacterHealth playerHealth;       // Reference to the player's heatlh.
     public GameObject enemy;                // The enemy prefab to be spawned.
     public float spawnTime = 3f;            // How long between each spawn.
-    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
-
+    //public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    //set custom range for random position
+    public float MinX = 0;
+    public float MaxX = 10;
+    public float MinY = 0;
+    public float MaxY = 10;
+    public float MinZ = 0;
+    public float MaxZ = 10;
 
     void Start()
     {
@@ -30,9 +36,15 @@ public class EnemyManager : MonoBehaviour {
         }
 
         // Find a random index between zero and one less than the number of spawn points.
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        //int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        float x = Random.Range(MinX, MaxX);
+        float y = Random.Range(MinY, MaxY);
+        float z = Random.Range(MinZ, MaxZ);
 
         // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        //Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        Instantiate(enemy, new Vector3(x, y, z), Quaternion.identity);
+
     }
 }
