ce8027b317784a062e56a64318faaf30ecb862c1
zoefishxd <xd.zoe.307@gmail.com>
2017-04-08 15:04:29 +0800

    fixed menus' script

---
+++ Assets/Script/MainMenuSetting.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSetting : MonoBehaviour {
+
+    public GameObject settingmenu;
+    public bool isShowing;
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (isShowing == false)
+        {
+            settingmenu.SetActive(false);
+        }
+    }*/
+
+    void Start()
+    {
+        settingmenu.SetActive(false);
+        isShowing = false;
+    }
+
+    public void openmenu()
+    {
+        settingmenu.SetActive(true);
+        isShowing = true;
+    }
+
+    public void closemenu()
+    {
+        settingmenu.SetActive(false);
+        isShowing = false;
+    }
+
+    public void mute(bool mutevalue)
+    {
+        //AudioListener.volume = 1;
+        //if(mutevalue)
+        if (mutevalue == true)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+        }
+    }
+
+    public void volumeslider(float vol)
+    {
+        AudioListener.volume = vol;
+    }
+
+}
