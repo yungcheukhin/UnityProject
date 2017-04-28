e043d630a648a881eca4c6aabe1206d6c88eb1dd
Ng Eris <fly3eris@hotmail.com>
2017-04-28 19:47:05 +0800

    Timer unfinished

---
+++ Assets/Script/TimerCountDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerCountDown : MonoBehaviour {
+
+    public float timeLeft = 300.0f;
+
+    public Text CountTimeText;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        CountTimeText.text = "Time Left:" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            
+        }
+    }
+}
