eef5ce7fd864a5178937b25adc16abb2851ebe59
yungcheukhin <yungcheukhin@gmail.com>
2017-05-05 15:31:48 +0800

    nearly all fixed

---
+++ Assets/Scripts/FixRoom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRoom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MazeRoom newRoom = ScriptableObject.CreateInstance<MazeRoom>();
+    }
+	
+}
